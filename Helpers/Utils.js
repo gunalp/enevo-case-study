d95ae742e99662bbf56371c1245930193347129b
Alp Uysal <gunalp.uysal@skysens.io>
2019-08-31 22:39:43 +0300

    game start && tile deal

---
+++ Helpers/Utils.js
@@ -0,0 +1,20 @@
+const numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ];
+const colors = ['M', 'Y', 'S', 'K'];
+const getAllTiles = () => {
+  const allTiles = [[0, 'S'],[0, 'S']];
+  for (let index = 0; index < colors.length; index++) {
+    const color = colors[index];
+    for (let index2 = 0; index2 < numbers.length; index2++) {
+      const number = numbers[index2];
+      allTiles.push([number, color]);
+      allTiles.push([number, color]);
+    }
+  }
+
+  return allTiles;
+}
+module.exports = {
+  numbers,
+  colors,
+  getAllTiles
+}
--- Init/WsServer.js
+++ Init/WsServer.js
@@ -1,4 +1,5 @@
 const WebSocketServer  = require("ws").Server;
+const Helpers = require('../Helpers/Utils');
 
 class WsServer {
 
@@ -25,6 +26,38 @@ class WsServer {
     }
   }
 
+  gameStart() {
+    this.brodcast(`Game is Starting`)
+
+    const firstPersonToken = Object.keys(this.Clients)[0];
+
+    for (var i in this.Clients) {
+      if (i === firstPersonToken ) {
+        this.Clients[i].send(JSON.stringify({
+          isTurn: true,
+          tiles: this.selectTiles(15)
+        }));
+      } else {
+        this.Clients[i].send(JSON.stringify({
+          isTurn: false,
+          tiles: this.selectTiles(14)
+        }));
+      }
+    }
+  }
+
+  selectTiles(tileCount) {
+    const tiles = [];
+    const allTiles = Helpers.getAllTiles()
+    for(let i = 0; i < tileCount; i++){
+      let tile = allTiles[Math.floor(Math.random() * allTiles.length)];
+      let order = allTiles.indexOf(tile);
+      allTiles.splice(order, 1);
+			tiles.push(tile);
+    }
+    return tiles;
+  }
+
   async start() {
     return new Promise(resolve => {
       this.wss.on('listening', () => {
@@ -33,17 +66,29 @@ class WsServer {
       })
 
       this.wss.on('connection', (ws) => {
-        const clientCount = Object.keys(this.Clients).length;
+                
         const token = this.getToken();
         this.Clients[token] = ws;
+        const clientCount = Object.keys(this.Clients).length;
+        console.log('init client Count', clientCount);
 
-        if (clientCount === 4) {
+        if (clientCount < 4) {
+          console.log('token', token, 'Client Count', Object.keys(this.Clients));
+          this.brodcast(`We Wait ${4 - Object.keys(this.Clients).length} Person `);
+
+        } else if (clientCount === 4) {
+
+         console.log('token', token, 'Client Count', Object.keys(this.Clients));
+         this.brodcast(`Game is starting! We have  ${Object.keys(this.Clients).length} Person `);
+         this.gameStart();
+        } else if (clientCount > 4) {
           console.log('clientCount', clientCount);
           this.Clients[token].send(`Sorry! Table is Full`)
           ws.close();
         } else {
-          console.log('token', token, 'Client Count', Object.keys(this.Clients));
-          this.brodcast(`We Wait ${4 - Object.keys(this.Clients).length} Person `);
+          console.log('another case', clientCount);
+          this.Clients[token].send(`Sorry! Table is Full`)
+          ws.close();
         }
 
         ws.on('close', () => {
